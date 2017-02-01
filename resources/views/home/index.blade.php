15f687d033600cbfb4cad80b244bb43125f8e5ef
Matt Crandell <mrcrandell@gmail.com>
2017-01-31 19:38:10 -0500

    Updated content on homepage and residential pages.

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -8,11 +8,6 @@
 		<img class="img-responsive featured-img" src="{{url('/')}}/img/home-img{{rand(1,5)}}.jpg" alt="Alchin's Disposal, Inc.">
 	</div>
 	<div class="col-sm-5 margin-bottom-20">
-		<div class="ribbon-container">
-			<p class="ribbon">
-			    <strong class="ribbon-content">Celebrating 30 years of Business</strong>
-			</p>
-		</div>
 
 		<p>Alchin's Disposal has been family owned since 1986, and is the only refuse removal company in Livingston County who can say this. Why give your money to a huge corporation when you can keep it local? Alchin’s strives on excellent customer service and can honestly say we stand behind our reputation of being the best refuse removal company in the Livingston County area.</p>
 	</div>
